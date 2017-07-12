d7503c1f99e65da4087a99213932d1ce8aeeb00e
kamil wroniewicz <qu4ku@hotmail.com>
2017-07-12 15:21:51 +0200

    index and navbar

--- dynamic/urls.py
+++ dynamic/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .views import index
+from django.conf import settings
+
+
 
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', admin.site.urls),
 ]
